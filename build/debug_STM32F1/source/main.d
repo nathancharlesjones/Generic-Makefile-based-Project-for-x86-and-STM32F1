52deb76b81aed137f123770a10da5e6a09be3d81
Nathan Jones <nathancharlesjones@gmail.com>
2020-07-22 19:24:23 -0400

    First commit

---
+++ build/debug_STM32F1/source/main.d
@@ -0,0 +1,4 @@
+build/debug_STM32F1/source/main.o: source/main.c \
+ hardware/include/hardwareAPI.h
+
+hardware/include/hardwareAPI.h:
---
+++ build/debug_x86/source/main.d
@@ -0,0 +1,4 @@
+build/debug_x86/source/main.o: source/main.c \
+ hardware/include/hardwareAPI.h
+
+hardware/include/hardwareAPI.h:
