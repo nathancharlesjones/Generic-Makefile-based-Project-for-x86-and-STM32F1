cdb422d5b6306d8c5f682e4101b604f04cd506b1
Nathan Jones <nathancharlesjones@gmail.com>
2020-07-22 19:30:22 -0400

    Removed build folder again.

--- build/debug_STM32F1/source/main.d
+++
@@ -1,4 +0,0 @@
-build/debug_STM32F1/source/main.o: source/main.c \
- hardware/include/hardwareAPI.h
-
-hardware/include/hardwareAPI.h:
--- build/debug_x86/source/main.d
+++
@@ -1,4 +0,0 @@
-build/debug_x86/source/main.o: source/main.c \
- hardware/include/hardwareAPI.h
-
-hardware/include/hardwareAPI.h:
